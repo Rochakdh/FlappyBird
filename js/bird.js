ad8b9dbe8dbe49d1ce8bb071039bab2e7416381e
Rochak Dhakal <rochakdh@gmail.com>
2021-05-14 11:04:03 +0545

    [added] pipes

--- js/bird.js
+++ js/bird.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-canvas.height = window.innerHeight;
+canvas.height = 515
 // canvas.width = window.innerWidth;
 c.font = "30px Arial";
 let startScreen = document.querySelector('.start-screen')
@@ -12,13 +12,35 @@ let backgroundX;
 let backgroundY;
 let backgrounHeight;
 let backgroundWidth = 335;
+let baseY;
+let slideBird = 0.3;
 let speed = 0.2;
+let baseHeight;
+let linerSpeed = 2;
+let jumpHeight = 20;
 
 class Pipes {
-    constructor(){
-        this.x = 0;
-        this. y = 0;
+    constructor(x){
+        this.x = x;
+        this.y = 0;
+        this.height = 0;
     };
+    
+
+    drawPipeBase = function(){
+        let pipeImage = new Image()
+        pipeImage.src = 'images/pipe.png';
+        let pipeX = backgroundX + this.x
+        let pipeY = canvas.height-baseHeight-pipeImage.height/2
+        c.drawImage(pipeImage,pipeX,pipeY,pipeImage.width,pipeImage.height/2)
+    }
+    drawPipeTop = function(){
+        let pipeImageUp = new Image()
+        pipeImageUp.src = 'images/pipeup.png';
+        let  pipeUpX = backgroundX + this.x
+        let pipeUpY = 0
+        c.drawImage(pipeImageUp,pipeUpX,pipeUpY,pipeImageUp.width,pipeImageUp.height/2)
+    }
 };
 
 background = function(){
@@ -33,31 +55,51 @@ background = function(){
 base = function() {
     let base = new Image()
     base.src = 'images/base.png';
-    let baseX = backgroundX - dx
-    let baseY = backgroundY+backgrounHeight-base.height
+    let baseX = backgroundX-speed
+    baseHeight = base.height
+    baseY = backgroundY+backgrounHeight-base.height
     speed += 0.5
     let baseWidth = backgroundWidth
     c.drawImage(base,baseX,baseY,2*baseWidth,base.height)
     if (baseX < backgroundX-backgroundWidth){
-        c.clearRect(0, 0, canvas.width, canvas.height);
+        // cancelAnimationFrame(animate)
         speed = 0
         baseX = backgroundX   
     }
-
 }
+
+let baseTop;
+let birdY;
+
 bird = function(){
-    
-
+    let bird = new Image()
+    bird.src = 'images/bird.png';
+    let birdX = backgroundX - 50 + backgroundWidth / 2; 
+    birdY = backgroundY - 100 + backgrounHeight / 2 + jumpHeight; 
+    c.drawImage(bird,birdX,birdY)
+    jumpHeight += linerSpeed
+    baseTop = Math.abs(canvas.height) - baseHeight - 20
+    if (birdY > baseTop){
+        // startScreen.style.display = 'none'
+        // canvas.style.display = 'none'
+        // endScreen.style.display = 'block'
+        // cancelAnimationFrame(animate)
+        // c.clearRect(0, 0, canvas.width, canvas.height);       
+    }
 }
+let pipes;
 function init(){
-    pipes = new Pipes()
+    pipes = new Pipes(200)
+    // pipes = new Pipes(200)
     
-
 }
 function animate (){
     background();
     base();
-    requestAnimationFrame(animate);
+    bird();
+    pipes.drawPipeBase()
+    pipes.drawPipeTop()
+    requestAnimationFrame(animate)
 }
 
 buttonStart.addEventListener('click', function(){
@@ -65,20 +107,19 @@ buttonStart.addEventListener('click', function(){
     endScreen.style.display = 'none'
     canvas.style.display = 'block'
     init();
-    animate()
+    animate();
 }
 );
 
 buttonHome.addEventListener('click', function(){
-    startScreen.style.display = 'block'
-    endScreen.style.display = 'none'
-    canvas.style.display = 'none'
-}
-);
+    location.reload()
+});
 
 document.addEventListener('keydown', (e) => {
     if (e.code === 'ArrowUp')
     {
-        console.log('check')
+        console.log(e)
+        jumpHeight -= 15 * linerSpeed
     }
 });
+
